ae8975e38ca4cf411cbf9038f6da5687f203b7e7
Yunfan <yfan2012@gmail.com>
2018-07-23 16:06:44 -0400

    copy illumina data to dilith

--- dunlop/dunlop.sh
+++ dunlop/dunlop.sh
@@ -17,3 +17,14 @@ if [ $1 == call ] ; then
     mkdir -p $datadir/call_done
     sbatch --array=0-693 --job-name=call_dunlop --output=$datadir/call_logs/dunlop_ecoli3.%A_%a.out $srcdir/bc_call.scr $datadir
 fi
+
+
+if [ $1 == illumina_dilith ] ; then
+    ##copy illumina data to dilithium
+    mkdir -p /dilithium/Data/NGS/Raw/180722_dunlop_3ecoli
+    for i in `find ~/BaseSpace/Projects/180722_dunlop_3ecoli/ -name *fastq.gz` ;
+    do
+	cp $i /dilithium/Data/NGS/Raw/180722_dunlop_3ecoli/
+    done
+fi
+
