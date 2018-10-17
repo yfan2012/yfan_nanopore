b8ffdc36bb7b080e6d111896ba67825ce9b3b6a3
Yunfan <yfan2012@gmail.com>
2018-10-17 01:58:29 -0400

    dunlop: 9 samp parsnp

--- dunlop/dunlop9.sh
+++ dunlop/dunlop9.sh
@@ -78,22 +78,13 @@ fi
 
 
 if [ $1 == parsnp ] ; then
-    parsdir=~/Dropbox/yfan/dunlop/parsnp
+    parsdir=~/Dropbox/yfan/dunlop/parsnp/ecoli9
     assembledir=~/Dropbox/yfan/dunlop/assemblies
     
-    mkdir -p $parsdir/raw_parsnp
-    mkdir -p $parsdir/canu17_parsnp
-    mkdir -p $parsdir/pilon_parsnp
-    mkdir -p $parsdir/pilon17_parsnp
+    mkdir -p $parsdir
 
-    parsnp -r $assembledir/ecoli_k12.fasta -d $assembledir/raw -p 12 -o $parsdir/raw_parsnp -c
-    harvesttools -i $parsdir/raw_parsnp/parsnp.ggr -V $parsdir/raw_parsnp/raw.vcf
-    parsnp -r $assembledir/ecoli_k12.fasta -d $assembledir/canu17 -p 12 -o $parsdir/canu17_parsnp -c
-    harvesttools -i $parsdir/canu17_parsnp/parsnp.ggr -V $parsdir/canu17_parsnp/canu17.vcf
-    parsnp -r $assembledir/ecoli_k12.fasta -d $assembledir/pilon -p 12 -o $parsdir/pilon_parsnp -c
-    harvesttools -i $parsdir/pilon_parsnp/parsnp.ggr -V $parsdir/pilon_parsnp/pilon.vcf
-    parsnp -r $assembledir/ecoli_k12.fasta -d $assembledir/pilon17 -p 12 -o $parsdir/pilon17_parsnp -c
-    harvesttools -i $parsdir/pilon17_parsnp/parsnp.ggr -V $parsdir/pilon17_parsnp/pilon17.vcf
+    parsnp -r $assembledir/bw25311.fasta -d $assembledir/ecoli9 -p 12 -o $parsdir -c
+    harvesttools -i $parsdir/parsnp.ggr -V $parsdir/raw.vcf
 fi
 
 if [ $1 == spades ] ; then
