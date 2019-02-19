fbde75a890ca924b5a751197df0d71a664c2904b
yfan2012 <yfan2012@gmail.com>
2019-02-19 04:37:30 -0500

    work on fungal assembly and polishing

--- birdpair/birdpair.sh
+++ birdpair/birdpair.sh
@@ -12,38 +12,43 @@ if [ $1 == untar ] ; then
     
 fi
 
-    
 if [ $1 == call ] ; then
     mkdir $datadir/called
     mkdir $datadir/call_logs
     mkdir $datadir/call_done
-    sbatch --array=0-141 --job-name=nina_call --output=$datadir/call_logs/nina_call.%A_%a.out $srcdir/bc_call.scr $datadir
+    sbatch --array=0-1356 --job-name=birdpair --output=$datadir/call_logs/bird_call.%A_%a.out $srcdir/bc_call_LSK109.scr $datadir
 fi
 
 if [ $1 == fastq ] ; then
-    mkdir $datadir/fastqs
-    cat $datadir/called/*/workspace/pass/barcode04/*.fastq > $datadir/fastqs/st90853.fastq
-    cat $datadir/called/*/workspace/pass/barcode05/*.fastq > $datadir/fastqs/st31.fastq
+    mkdir -p $datadir/fastqs
+    cat $datadir/called/*/workspace/pass/barcode09/*.fastq > $datadir/fastqs/patient.fastq
+    cat $datadir/called/*/workspace/pass/barcode10/*.fastq > $datadir/fastqs/bird.fastq
 fi
 
-if [ $1 == assemble_90853 ] ; then
-    mkdir -p $datadir/st90853_assembly
-    ~/software/canu-1.7/Linux-amd64/bin/canu \
-	-p st90853 -d $datadir/assembly \
-	-gridOptions="--mem=8g --time=22:00:00 --account=mschatz1" \
-	-gnuplotTested=True \
-	-minReadLength=500 \
-	genomeSize=39m \
-	-nanopore-raw $datadir/fastqs/st90853.fastq
+if [ $1 == long_fq ] ; then
+    python ~/Code/utils/fastq_long.py -i $datadir/fastqs/patient.fastq -o $datadir/fastqs/patient_over5kb.fastq -l 5000
+    python ~/Code/utils/fastq_long.py -i $datadir/fastqs/bird.fastq -o $datadir/fastqs/bird_over5kb.fastq -l 5000
 fi
 
-if [ $1 == assemble_31 ] ; then
-    mkdir -p $datadir/st31_assembly
-    ~/software/canu-1.7/Linux-amd64/bin/canu \
-	-p st31 -d $datadir/st31_assembly \
-	-gridOptions="--mem=8g --time=22:00:00 --account=mschatz1" \
-	-gnuplotTested=True \
-	-minReadLength=500 \
-	genomeSize=39m \
-	-nanopore-raw $datadir/fastqs/st31.fastq
+if [ $1 == wtdbg2 ] ; then
+    mkdir -p $datadir/patient_wtdbg2
+    mkdir -p $datadir/bird_wtdbg2
+    wtdbg2 -t 32 -i $datadir/fastqs/patient_over5kb.fastq -fo $datadir/patient_wtdbg2/patient_wtdbg2
+    wtdbg2 -t 32 -i $datadir/fastqs/bird_over5kb.fastq -fo $datadir/bird_wtdbg2/bird_wtdbg2
+    wtpoa-cns -t 32 -i $datadir/patient_wtdbg2/patient_wtdbg2.ctg.lay.gz -fo $datadir/patient_wtdbg2/patient_wtdbg2.contigs.fasta
+    wtpoa-cns -t 32 -i $datadir/bird_wtdbg2/bird_wtdbg2.ctg.lay.gz -fo $datadir/bird_wtdbg2/bird_wtdbg2.contigs.fasta
+fi
+
+if [ $1 == pilon ] ; then
+    ##sed -i -e 's/ /_/g' $datadir/bird_wtdbg2/bird_wtdbg2.contigs.fasta
+    ##sed -i -e 's/ /_/g' $datadir/patient_wtdbg2/patient_wtdbg2.contigs.fasta
+    
+    mkdir -p $datadir/pilon_patient
+    mkdir -p $datadir/pilon_bird
+
+    cp /work-zfs/mschatz1/cpowgs/fungus/illumina/patient* $datadir/pilon_patient/
+    cp /work-zfs/mschatz1/cpowgs/fungus/illumina/bird* $datadir/pilon_bird/ 
+    
+    sbatch --output=$datadir/batch_logs/bird_pilon.out --job-name=bird ./pilon.scr $datadir/pilon_bird $datadir/bird_wtdbg2/bird_wtdbg2.contigs.fasta bird wtdbg2
+    sbatch --output=$datadir/batch_logs/patient_pilon.out --job-name=patient ./pilon.scr $datadir/pilon_patient $datadir/patient_wtdbg2/patient_wtdbg2.contigs.fasta patient wtdbg2
 fi
--- nina/fungus2.sh
+++ nina/fungus2.sh
@@ -1,15 +1,9 @@
 #!/bin/bash
 
 srcdir=~/Code/utils/marcc
-datadir=/scratch/groups/mschatz1/cpowgs/fungus/
+datadir=/scratch/groups/mschatz1/cpowgs/fungus
 
 
-if [ $1 == untar ] ; then
-    ##NB manually moved stuff to mike's storage allocation. These paths may be wrong
-    mkdir $datadir/raw
-    mkdir $datadir/batch_logs
-    sbatch --output=$datadir/batch_logs/untar.out --job-name=ut_nina $srcdir/untar.scr $datadir/180827_nina_fungus2.tar.gz $datadir
-fi
 
 if [ $1 == untar_v2 ] ; then
     mkdir -p $datadir/181108_nina_v2
@@ -20,12 +14,7 @@ if [ $1 == untar_v2 ] ; then
     bash $srcdir/untar.scr $datadir/181108_nina_v2.tar.gz $datadir/181108_nina_v2
 fi
     
-if [ $1 == call ] ; then
-    mkdir $datadir/called
-    mkdir $datadir/call_logs
-    mkdir $datadir/call_done
-    sbatch --array=0-141 --job-name=nina_call --output=$datadir/call_logs/nina_call.%A_%a.out $srcdir/bc_call.scr $datadir
-fi
+
 
 if [ $1 == call_v2 ] ; then
     mkdir -p $datadir/181108_nina_v2/called
@@ -34,11 +23,7 @@ if [ $1 == call_v2 ] ; then
     sbatch --array=0-1901 --job-name=nina_call --output=$datadir/181108_nina_v2/call_logs/nina_call.%A_%a.out $srcdir/bc_call_LSK109.scr $datadir/181108_nina_v2
 fi
 
-if [ $1 == fastq ] ; then
-    mkdir $datadir/fastqs
-    cat $datadir/called/*/workspace/pass/barcode04/*.fastq > $datadir/fastqs/st90853.fastq
-    cat $datadir/called/*/workspace/pass/barcode05/*.fastq > $datadir/fastqs/st31.fastq
-fi
+
 
 if [ $1 == fastq_v2 ] ; then
     mkdir -p $datadir/181108_nina_v2/fastqs/
@@ -59,16 +44,6 @@ if [ $1 == assemble_90853_v2 ] ; then
 fi
 
 
-if [ $1 == assemble_90853 ] ; then
-    mkdir -p $datadir/st90853_assembly
-    ~/software/canu-1.7/Linux-amd64/bin/canu \
-	-p st90853 -d $datadir/assembly \
-	-gridOptions="--mem=8g --time=22:00:00 --account=mschatz1" \
-	-gnuplotTested=True \
-	-minReadLength=500 \
-	genomeSize=39m \
-	-nanopore-raw $datadir/fastqs/st90853.fastq
-fi
 
 if [ $1 == assemble_31_v2 ] ; then
     mkdir -p $datadir/181108_nina_v2/st31_assembly
@@ -77,16 +52,57 @@ if [ $1 == assemble_31_v2 ] ; then
 	-gridOptions="--time=22:00:00 --account=mschatz1 --partition=parallel" \
 	genomeSize=39m \
 	stopOnReadQuality=false \
-	-nanopore-raw $datadir/181108_nina_v2/fastqs/st31.fastq
+	-nanopore-raw $datadir/181108_nina_v2/fastqs/st31_bothruns_over3kb.fastq
 fi
 
-if [ $1 == assemble_31 ] ; then
-    mkdir -p $datadir/st31_assembly
-    ~/software/canu-1.7/Linux-amd64/bin/canu \
-	-p st31 -d $datadir/st31_assembly \
-	-gridOptions="--mem=8g --time=22:00:00 --account=mschatz1" \
-	-gnuplotTested=True \
-	-minReadLength=500 \
-	genomeSize=39m \
-	-nanopore-raw $datadir/fastqs/st31.fastq
+
+if [ $1 == merge_fq ] ; then
+    cat $datadir/181108_nina_v2/fastqs/st90853.fastq $datadir/180827_nina_fungus2/fastqs/st90853.fastq > $datadir/181108_nina_v2/fastqs/st90853_bothruns.fastq
+    cat $datadir/181108_nina_v2/fastqs/st31.fastq $datadir/180827_nina_fungus2/fastqs/st31.fastq > $datadir/181108_nina_v2/fastqs/st31_bothruns.fastq
+fi
+    
+if [ $1 == long_fq ] ; then
+    python ~/Code/utils/fastq_long.py -i $datadir/181108_nina_v2/fastqs/st31_bothruns.fastq -o $datadir/181108_nina_v2/fastqs/st31_bothruns_over3kb.fastq -l 3000
+    python ~/Code/utils/fastq_long.py -i $datadir/181108_nina_v2/fastqs/st90853_bothruns.fastq -o $datadir/181108_nina_v2/fastqs/st90853_bothruns_over3kb.fastq -l 3000
+fi
+
+if [ $1 == assemble_90853_wtdbg2 ] ; then
+    mkdir -p $datadir/181108_nina_v2/st90853_wtdbg2
+    wtdbg2 -t 32 -i $datadir/181108_nina_v2/fastqs/st90853_bothruns_over3kb.fastq -fo $datadir/181108_nina_v2/st90853_wtdbg2/st90853_wtdbg2
+    wtpoa-cns -t 32 -i $datadir/181108_nina_v2/wtdbg2/st90853_wtdbg2.ctg.lay.gz -fo $datadir/181108_nina_v2/st90853_wtdbg2/st90853.wtdbg2.contigs.fasta
+fi
+
+if [ $1 == assemble_31_wtdbg2 ] ; then
+    mkdir -p $datadir/181108_nina_v2/st31_wtdbg2
+    wtdbg2 -t 32 -i $datadir/181108_nina_v2/fastqs/st31_bothruns_over3kb.fastq -fo $datadir/181108_nina_v2/st31_wtdbg2/st31_wtdbg2
+    wtpoa-cns -t 32 -i $datadir/181108_nina_v2/st31_wtdbg2/st31_wtdbg2.ctg.lay.gz -fo $datadir/181108_nina_v2/st31_wtdbg2/st31.wtdbg2.contigs.fasta
+fi
+
+
+if [ $1 == pilon ] ; then
+    ##sed -i -e 's/ /_/g' $datadir/181108_nina_v2/st31_wtdbg2/st31.wtdbg2.contigs.fasta
+    ##sed -i -e 's/ /_/g' $datadir/181108_nina_v2/st90853_wtdbg2/st90853.wtdbg2.contigs.fasta
+    
+    mkdir -p $datadir/181108_nina_v2/pilon_st90853
+    mkdir -p $datadir/181108_nina_v2/pilon_st31
+
+    ##cp /work-zfs/mschatz1/cpowgs/fungus/illumina/st31* $datadir/181108_nina_v2/pilon_st31/
+    ##cp /work-zfs/mschatz1/cpowgs/fungus/illumina/st90853* $datadir/181108_nina_v2/pilon_st90853/
+    
+    sbatch --output=$datadir/181108_nina_v2/batch_logs/st31_wtdbg2.out --job-name=st31_wtdbg2 ./pilon.scr $datadir/181108_nina_v2/pilon_st31 $datadir/181108_nina_v2/st31_wtdbg2/st31.wtdbg2.contigs.fasta st31 wtdbg2
+    sbatch --output=$datadir/181108_nina_v2/batch_logs/st90853_wtdbg2.out --job-name=st90853_wtdbg2 ./pilon.scr $datadir/181108_nina_v2/pilon_st90853 $datadir/181108_nina_v2/st90853_wtdbg2/st90853.wtdbg2.contigs.fasta st90853 wtdbg2
+fi
+
+if [ $1 == pilon2 ] ; then
+    ##sed -i -e 's/ /_/g' $datadir/181108_nina_v2/st31_wtdbg2/st31.wtdbg2.contigs.fasta
+    ##sed -i -e 's/ /_/g' $datadir/181108_nina_v2/st90853_wtdbg2/st90853.wtdbg2.contigs.fasta
+    
+    mkdir -p $datadir/181108_nina_v2/pilon_st90853
+    mkdir -p $datadir/181108_nina_v2/pilon_st31
+
+    ##cp /work-zfs/mschatz1/cpowgs/fungus/illumina/st31* $datadir/181108_nina_v2/pilon_st31/
+    ##cp /work-zfs/mschatz1/cpowgs/fungus/illumina/st90853* $datadir/181108_nina_v2/pilon_st90853/
+    
+    sbatch --output=$datadir/181108_nina_v2/batch_logs/st31_wtdbg2.out --job-name=st31_wtdbg2 ./pilon2.scr $datadir/181108_nina_v2/pilon_st31 $datadir/181108_nina_v2/st31_wtdbg2/st31.wtdbg2.contigs.fasta st31 wtdbg2
+    sbatch --output=$datadir/181108_nina_v2/batch_logs/st90853_wtdbg2.out --job-name=st90853_wtdbg2 ./pilon2.scr $datadir/181108_nina_v2/pilon_st90853 $datadir/181108_nina_v2/st90853_wtdbg2/st90853.wtdbg2.contigs.fasta st90853 wtdbg2
 fi
