73055d33b5b84af59c46e124ec7332dfdc1f4764
yfan2012 <yfan2012@gmail.com>
2020-08-05 15:52:41 -0400

    start code to classify reads

--- mdr/kraken2.sh
+++ mdr/kraken2.sh
@@ -6,3 +6,12 @@ if [ $1 == builddb ] ; then
     kraken2-build --standard --threads 12 --kmer-len 28 --minimizer-len 24 --minimizer-spaces 6 --use-ftp --db $dbdir/standard
     ##https://github.com/DerrickWood/kraken2/issues/226
 fi
+
+datadir=/uru/Data/Nanopore/projects/mdr/MDRstool_16
+
+if [ $1 == runkraken ] ; then
+    natfq=$datadir/fastqs/MDRstool_16_native.fq
+    pcrfq=$datadir/fastqs/MDRstool_16_pcr.fq
+    illfq=$datadir
+	
+    
