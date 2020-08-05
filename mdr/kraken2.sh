bb846ffe013e4c6a55c356c3ccdc2273c9990fb4
Yunfan <yfan2012@gmail.com>
2020-08-05 15:04:29 -0400

    build kraken2 database

--- mdr/kraken2.sh
+++ mdr/kraken2.sh
@@ -3,6 +3,6 @@
 dbdir=/mithril/Data/Nanopore/ref/kraken2
 
 if [ $1 == builddb ] ; then
-    kraken2-build --standard --threads 12 --db $dbdir/standard
+    kraken2-build --standard --threads 12 --kmer-len 28 --minimizer-len 24 --minimizer-spaces 6 --use-ftp --db $dbdir/standard
     ##https://github.com/DerrickWood/kraken2/issues/226
 fi
