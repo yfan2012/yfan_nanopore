0930eefe03be6feac0a2cba6fcdce45d2cf845dc
yfan2012 <yfan2012@gmail.com>
2021-06-04 16:40:24 -0400

    start barcoding mdr samp with faster script that does NA

--- mdr/mdr/barcode.sh
+++ mdr/mdr/barcode.sh
@@ -5,12 +5,9 @@ datadir=/mithril/Data/Nanopore/projects/methbin/disco
 prefix=200708_mdr_stool16native
 
 if [ $1 == megaidx ] ; then
-    for i in $samps ;
-    do
-	python3 ~/Code/yfan_meth/utils/megalodon_mod_basecalls_idx.py \
-		-i $ssddir/megalodon/$i/per_read_modified_base_calls.txt \
-		-o $ssddir/megalodon/$i/per_read_modified_base_calls.txt.idx
-    done
+    python3 ~/Code/yfan_meth/utils/megalodon_mod_basecalls_idx.py \
+	    -i $ssddir/megalodon/$prefix/per_read_modified_base_calls.txt \
+	    -o $ssddir/megalodon/$prefix/per_read_modified_base_calls.txt.idx
 fi
 
 
@@ -38,7 +35,6 @@ if [ $1 == barcode ] ; then
                -r $ref \
                -b ~/Code/yfan_nanopore/mdr/disco/disco_barcodes.txt \
                -o $ssddir/barcode/$i/${i}_barcodes.txt \
-	       -n 100000000000 \
                -t 36 ;} &> $ssddir/barcode/$i/${i}_time.txt
     done
 fi
