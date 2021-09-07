15b0f1cdf4d8ee961904461bb23bbdfc22a6f894
yfan2012 <yfan2012@gmail.com>
2021-09-07 16:17:12 -0400

    plot reads classified by megalodon to asm

--- mdr/zymo/cluster_polished.R
+++ mdr/zymo/cluster_polished.R
@@ -6,7 +6,7 @@ source('~/Code/yfan_nanopore/mdr/mdr/cluster_functions.R')
 
 
 datadir='/mithril/Data/Nanopore/projects/methbin/zymo'
-dbxdir='~/gdrive/mdr/mdr'
+dbxdir='~/gdrive/mdr/zymo'
 prefix='20190809_zymo_control_polished'
 
 barcodelist=file.path('~/Code/yfan_nanopore/mdr/rebase/barcodes15.txt')
@@ -30,20 +30,63 @@ nacount=colSums(is.na(fullbcinfo)/dim(fullbcinfo)[1])
 lowna=nacount[nacount<.2]
 plasnacount=colSums(is.na(plasbcinfo)/dim(plasbcinfo)[1])
 plaslowna=plasnacount[plasnacount<.2]
-
 keepmotifs=intersect(names(lowna), names(plaslowna))
+
 
 bcinfo=fullbcinfo %>%
     select(all_of(keepmotifs))
 bccounts=fullbccounts %>%
     select(all_of(keepmotifs)) %>%
-    filter(across(c(-readname, -chrname), ~.x>=3))
+    filter(complete.cases(.)) %>%
+    filter(across(c(-readname, -chrname), ~.x>=5)) %>%
+    filter(chrname %in% key$tig) %>%
+    rowwise() %>%
+    mutate(chrname=key$species[key$tig==chrname])
 
-bcfilt=bcinfo %>%
+allbcfilt=bcinfo %>%
+    filter(complete.cases(.)) %>%
     filter(readname %in% bccounts$readname) %>%
     filter(chrname %in% key$tig) %>%
     rowwise() %>%
     mutate(chrname=key$species[key$tig==chrname])
- 
-plasinfo=bcfilt %>%
+
+plasinfo=allbcfilt %>%
     filter(chrname %in% plasnames)
+
+bcfilt=allbcfilt %>%
+    filter(!(chrname %in% plasnames)) %>%
+    group_by(chrname) %>%
+    do(checkfilt(., 10000, bccounts)) %>%
+    ungroup()
+
+bcdata=rbind(bcfilt, plasinfo)
+
+
+
+bcumap=umap(bcdata %>% select(-chrname, -readname))
+
+embedinfo=tibble(x=bcumap$layout[,1],
+             y=bcumap$layout[,2],
+             label=bcdata$chrname)
+embedchr=embedinfo %>%
+    filter(!(label %in% plasnames))
+embedplas=embedinfo %>%
+    filter(label %in% plasnames)
+
+
+
+mycolors=c(brewer.pal(8, 'Set2'), '#000000')
+myshapes=c(3,18)
+plasfile=file.path(dbxdir, 'plasmid_shown_polished.pdf')
+pdf(plasfile, h=9, w=13)
+plot=ggplot(embedchr, aes(x=x, y=y, colour=label)) +
+    geom_point(alpha=.2, size=.1) +
+    scale_colour_manual(values=mycolors) +
+    theme_bw()
+mainplot=plot +
+    geom_point(data=embedplas, aes(x=x, y=y, shape=label), inherit.aes=FALSE) +
+    scale_shape_manual(values=myshapes) +
+    theme(legend.position='none')
+print(mainplot)
+dev.off()
+
