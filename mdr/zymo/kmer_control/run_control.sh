a3fea43ac7798789fb56b22bccdd0ae3c6a4c86f
yfan2012 <yfan2012@gmail.com>
2021-10-12 02:21:52 -0400

    run motif shuffling control

--- mdr/zymo/kmer_control/run_control.sh
+++ mdr/zymo/kmer_control/run_control.sh
@@ -4,7 +4,7 @@ prefix=20190809_zymo_control
 datadir=/mithril/Data/Nanopore/projects/methbin/zymo
 ref=/uru/Data/Nanopore/projects/read_class/zymo/ref/zymo_all.fa
 
-if [ $1 == test ] ; then
+if [ $1 == test_kmer ] ; then
     mkdir -p $datadir/kmer_control
 
     { time python ~/Code/yfan_meth/utils/megalodon_barcode_kmer_control.py \
@@ -16,7 +16,7 @@ if [ $1 == test ] ; then
            -t 12 ;} &> $datadir/kmer_control/test_time.txt
 fi
     
-if [ $1 == runcontrol ] ; then
+if [ $1 == run_control ] ; then
     mkdir -p $datadir/kmer_control/$prefix
 
     { time python ~/Code/yfan_meth/utils/megalodon_barcode_kmer_control.py \
@@ -26,4 +26,29 @@ if [ $1 == runcontrol ] ; then
            -b ~/Code/yfan_nanopore/mdr/rebase/barcodes20.txt \
            -o $datadir/kmer_control/$prefix/${prefix}_barcodes20.txt \
            -t 12 ;} &> $datadir/kmer_control/$prefix/${prefix}_time20.txt
+fi
+
+
+if [ $1 == test_motifs ] ; then
+    mkdir -p $datadir/kmer_control
+
+    { time python ~/Code/yfan_meth/utils/megalodon_barcode_motif_control.py \
+           -m $datadir/megalodon/$prefix/per_read_modified_base_calls.txt \
+           -i $datadir/megalodon/$prefix/per_read_modified_base_calls.txt.small.idx \
+           -r $ref \
+           -b ~/Code/yfan_nanopore/mdr/rebase/barcodes20.txt \
+           -o $datadir/kmer_control/test.txt \
+           -t 12 ;} &> $datadir/kmer_control/test_time.txt
+fi
+
+if [ $1 == run_motifs ] ; then
+    mkdir -p $datadir/kmer_control/${prefix}_motif
+
+    { time python ~/Code/yfan_meth/utils/megalodon_barcode_motif_control.py \
+           -m $datadir/megalodon/$prefix/per_read_modified_base_calls.txt \
+           -i $datadir/megalodon/$prefix/per_read_modified_base_calls.txt.idx \
+           -r $ref \
+           -b ~/Code/yfan_nanopore/mdr/rebase/barcodes20.txt \
+           -o $datadir/kmer_control/${prefix}_motif/${prefix}_barcodes20.txt \
+           -t 12 ;} &> $datadir/kmer_control/${prefix}_motif/${prefix}_time20.txt
 fi
