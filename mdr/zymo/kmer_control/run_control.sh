c076c0c9211dc7292efcd25331748832a02e23e0
yfan2012 <yfan2012@gmail.com>
2021-10-13 18:55:07 -0400

    motif and kmer tests. check out a log score

--- mdr/zymo/kmer_control/run_control.sh
+++ mdr/zymo/kmer_control/run_control.sh
@@ -50,5 +50,19 @@ if [ $1 == run_motifs ] ; then
            -r $ref \
            -b ~/Code/yfan_nanopore/mdr/rebase/barcodes20.txt \
            -o $datadir/kmer_control/${prefix}_motif/${prefix}_barcodes20.txt \
+	   -s $datadir/kmer_control/${prefix}_motif/shuffled_barcodes20.txt \
            -t 12 ;} &> $datadir/kmer_control/${prefix}_motif/${prefix}_time20.txt
 fi
+
+if [ $1 == run_log ] ; then
+    mkdir -p $datadir/kmer_control/${prefix}_log
+
+    { time python ~/Code/yfan_meth/utils/megalodon_barcode_log.py \
+           -m $datadir/megalodon/$prefix/per_read_modified_base_calls.txt \
+           -i $datadir/megalodon/$prefix/per_read_modified_base_calls.txt.idx \
+           -r $ref \
+           -b ~/Code/yfan_nanopore/mdr/rebase/barcodes20.txt \
+           -o $datadir/kmer_control/${prefix}_log/${prefix}_barcodes20.txt \
+           -t 12 ;} &> $datadir/kmer_control/${prefix}_log/${prefix}_time20.txt
+fi
+
