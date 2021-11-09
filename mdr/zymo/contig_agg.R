51779f0a82cb0bb294e3e023a5b7521044fec64b
yfan2012 <yfan2012@gmail.com>
2021-11-09 11:11:07 -0500

    wirte out motifs per contig called by megalodon

--- mdr/zymo/contig_agg.R
+++ mdr/zymo/contig_agg.R
@@ -22,3 +22,33 @@ plot=ggplot(summary, aes(x=methfrac, colour=chr, fill=chr, alpha=.3)) +
     theme_bw()
 print(plot)
 dev.off()
+
+
+barcodefile='~/Code/yfan_nanopore/mdr/rebase/barcodes50.txt'
+bc=read_tsv(barcodefile, col_names=c('motif'))
+bc_cols=c('chr', bc$motif)
+
+motifinfofile=file.path(datadir, '20190809_zymo_control_barcodes50.csv')
+motifinfo=read_csv(motifinfofile, col_names=bc_cols, na=c("", "None"))
+
+summary=c()
+for (i in 1:dim(motifinfo)[1]) {
+    row=motifinfo[i,][-1]
+    row[is.na(row)]=0
+    a=row[as.vector(row>.8)]
+    if (length(a)>0) {
+        motifs=names(a)
+        str=''
+        for (j in 1:length(a)) {
+            str=paste0(str, '\t', motifs[j], ',', as.character(a[j]))
+        }
+        info=paste0(motifinfo[i,1],str)
+    }else{
+        info=paste0(motifinfo[i,1])
+    }
+    summary=c(summary, info)
+}
+
+
+summaryfile=file.path(datadir, '20190809_zymo_control_motifs.txt')
+write(summary, summaryfile)
--- mdr/zymo/contig_agg.sh
+++ mdr/zymo/contig_agg.sh
@@ -9,7 +9,7 @@ if [ $1 == test ] ; then
     mkdir -p $datadir
     mkdir -p $datadir/test
 
-    time ( python ~/Code/yfan_meth/utils/megalodon_agg_experimental.py \
+    time ( python ~/Code/yfan_meth/utils/megalodon_agg.py \
 	   -m $projdir/megalodon/$prefix/per_read_modified_base_calls.txt \
 	   -i $projdir/megalodon/$prefix/per_read_modified_base_calls.txt.small.idx \
 	   -r $ref \
@@ -31,3 +31,21 @@ if [ $1 == agg ] ; then
 	   -v
 fi
 
+
+if [ $1 == findmotifs ] ; then
+    mkdir -p $datadir/$prefix
+
+    while read p; do
+        label=`echo $p | cut -d ' ' -f 2`
+        chrom=`echo $p | cut -d ' ' -f 1`
+	python ~/Code/yfan_meth/utils/bismark_motif_finder.py \
+	       -c $datadir/$prefix/$prefix.meth_report.txt \
+	       -r $ref \
+	       -b ~/Code/yfan_nanopore/mdr/rebase/barcodes50.txt \
+	       -p .5 \
+	       -l 8 \
+	       -m 10 \
+	       -s $chrom >> $datadir/$prefix/${prefix}_barcodes50.csv
+	done < ./truth/chrlist_withlabels.txt
+fi
+    
