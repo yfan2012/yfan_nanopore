ad06cd27952f3b7f80b220c8570c899a6a3c006b
yfan2012 <yfan2012@gmail.com>
2021-11-16 15:20:01 -0500

    check megalodon's aggregation results

--- mdr/zymo/contig_agg.R
+++ mdr/zymo/contig_agg.R
@@ -1,9 +1,14 @@
 library(tidyverse)
+library(RColorBrewer)
 
-datadir='/mithril/Data/Nanopore/projects/methbin/zymo/contig_agg/20190809_zymo_control'
-sumfile=file.path(datadir, '20190809_zymo_control.meth_report.txt')
+prefix='20190809_zymo_control'
+projdir='/mithril/Data/Nanopore/projects/methbin/zymo'
+datadir=file.path(projdir, 'contig_agg', prefix)
+sumfile=file.path(datadir, paste0(prefix, '.meth_report.txt'))
 dbxdir='~/gdrive/mdr/zymo'
 
+
+####plot methfracs from aggregation of meth text file
 sumfilecols=c('chr', 'pos', 'meth', 'unmeth')
 
 summary=read_tsv(sumfile, col_names=sumfilecols) %>%
@@ -24,6 +29,7 @@ print(plot)
 dev.off()
 
 
+####write out summary of putative motifs
 barcodefile='~/Code/yfan_nanopore/mdr/rebase/barcodes50.txt'
 bc=read_tsv(barcodefile, col_names=c('motif'))
 bc_cols=c('chr', bc$motif)
@@ -49,6 +55,53 @@ for (i in 1:dim(motifinfo)[1]) {
     summary=c(summary, info)
 }
 
-
 summaryfile=file.path(datadir, '20190809_zymo_control_motifs.txt')
 write(summary, summaryfile)
+
+
+####plot meth fracs from megalodon aggregation
+megacols=c('chrom', 'start', 'end', 'name', 'score', 'strand', 'tstart', 'tend', 'color', 'cov', 'methfrac')
+mycolors=colorRampPalette(brewer.pal(8, 'Set2'))(9)
+
+cmethfile=file.path(projdir, 'megalodon', prefix, 'modified_bases.5mC.bed')
+cmeth=read_tsv(cmethfile, col_names=megacols) %>%
+    filter(!grepl('tig', chrom, fixed=TRUE)) %>%
+    filter(cov>15)
+
+amethfile=file.path(projdir, 'megalodon', prefix, 'modified_bases.6mA.bed')
+ameth=read_tsv(amethfile, col_names=megacols) %>%
+    filter(!grepl('tig', chrom, fixed=TRUE)) %>%
+    filter(cov>15)
+
+allmeth=bind_rows(ameth, cmeth)
+
+aggmegapdf=file.path(dbxdir, 'agg_by_mega_methfracs.pdf')
+pdf(aggmegapdf, w=15, h=8)
+plot=ggplot(allmeth, aes(x=methfrac, colour=chrom, fill=chrom, alpha=.3)) +
+    geom_histogram() +
+    scale_y_log10()+
+    ggtitle('All meth') +
+    scale_colour_manual(values=mycolors) +
+    scale_fill_manual(values=mycolors) +
+    facet_wrap(~chrom) +
+    theme_bw()
+print(plot)
+aplot=ggplot(ameth, aes(x=methfrac, colour=chrom, fill=chrom, alpha=.3)) +
+    geom_histogram() +
+    scale_y_log10()+
+    ggtitle('A meth') +
+    scale_colour_manual(values=mycolors) +
+    scale_fill_manual(values=mycolors) +
+    facet_wrap(~chrom) +
+    theme_bw()
+print(aplot)
+cplot=ggplot(cmeth, aes(x=methfrac, colour=chrom, fill=chrom, alpha=.3)) +
+    geom_histogram() +
+    scale_y_log10()+
+    ggtitle('C meth') +
+    scale_colour_manual(values=mycolors) +
+    scale_fill_manual(values=mycolors) +
+    facet_wrap(~chrom) +
+    theme_bw()
+print(cplot)
+dev.off()
