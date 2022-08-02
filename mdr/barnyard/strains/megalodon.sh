f219e549b25ed7a8f54165fc810bda414038ce51
yfan2012 <yfan2012@gmail.com>
2022-08-02 17:12:43 -0400

    generate modbam

--- mdr/barnyard/strains/megalodon.sh
+++ mdr/barnyard/strains/megalodon.sh
@@ -48,3 +48,34 @@ if [ $1 == megaidx ] ; then
 		-o $datadir/megalodon/$samp/per_read_modified_base_calls.txt.idx
     done
 fi
+
+
+if [ $1 == modbams ] ; then
+    mkdir -p $ssddir/megalodon
+
+    for i in $samps ;
+    do
+	samp=${prefix}$i
+	mkdir -p $ssddir/megalodon/$samp
+	
+	megalodon \
+	    $ssddir/raw/$samp/no_sample/*/fast5_pass \
+	    --overwrite \
+	    --guppy-server-path "/usr/bin/guppy_basecall_server" \
+	    --guppy-params "-d /home/yfan/software/rerio/basecall_models/ --num_callers 5 --ipc_threads 6" \
+	    --guppy-config res_dna_r941_min_modbases-all-context_v001.cfg \
+	    --reference $ref \
+	    --outputs mod_mappings \
+	    --output-directory $ssddir/megalodon/$samp \
+	    --devices "cuda:0" \
+	    --processes 36
+	rm $ssddir/megalodon/$samp/per_read_modified_base_calls.db
+    done
+
+    mkdir -p $datadir/megalodon
+    for i in $samps ;
+    do
+	samp=${prefix}$i
+	mv $ssddir/megalodon/$samp/* $datadir/megalodon/$samp/
+    done
+fi
