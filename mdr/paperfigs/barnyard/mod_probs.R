c7b12bd1dbea7be245d4d6520d85e31e5e5a2e89
yfan2012 <yfan2012@gmail.com>
2022-08-10 00:02:59 -0400

    add lines to prob plots and write out table

--- mdr/paperfigs/barnyard/mod_probs.R
+++ mdr/paperfigs/barnyard/mod_probs.R
@@ -25,19 +25,42 @@ daminfo=methprobs %>%
 dcminfo=methprobs %>%
     filter(motif=='dcm')
 
+dbxdir='~/gdrive/mdr/paperfigs/figs'
 
-modprobdistpdf=file.path(dbxdir, 'methprobs.pdf')
+modprobdistpdf=file.path(dbxdir, 'barnyard_methprobs.pdf')
 pdf(modprobdistpdf, w=11, h=8)
 damplot=ggplot(daminfo, aes(x=modprob, colour=samp, fill=samp, alpha=.1)) +
     geom_density() +
-    ggtitle('dam') +
+    ggtitle('dam') +    
     facet_wrap(~pos) +
-    theme_bw()
+    geom_vline(xintercept=.2) +
+    geom_vline(xintercept=.8) +
+    theme_bw() +
+    theme(panel.grid.minor = element_blank())
 print(damplot)
 dcmplot=ggplot(dcminfo, aes(x=modprob, colour=samp, fill=samp, alpha=.1)) +
     geom_density() +
     ggtitle('dcm') +
     facet_wrap(~pos) +
-    theme_bw()
+    geom_vline(xintercept=.2) +
+    geom_vline(xintercept=.8) +
+    theme_bw() +
+    theme(panel.grid.minor = element_blank())
 print(dcmplot)
 dev.off()
+
+
+dam.midcounts=daminfo %>%
+    group_by(chrom, pos, samp) %>%
+    summarise(middle=sum(modprob<.8 & modprob>.2)/n()) %>%
+    mutate(motif='GATC')
+dcm.midcounts=dcminfo %>%
+    group_by(chrom, pos, samp) %>%
+    summarise(middle=sum(modprob<.8 & modprob>.2)/n()) %>%
+    mutate(motif='CCWGG')
+
+midcounts=rbind(dam.midcounts, dcm.midcounts)
+midcountscsv=file.path(dbxdir, 'barnyard_methprobs.csv')
+write_csv(midcounts, midcountscsv)
+
+    
